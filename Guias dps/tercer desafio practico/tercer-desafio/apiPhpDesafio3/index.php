3e227ec1b04725d2077c33edf6200250512b404c
oscarArdon <cdb5aardon4@gmail.com>
2020-11-09 19:59:20 -0600

    archivos php correctos

--- Guias dps/tercer desafio practico/tercer-desafio/apiPhpDesafio3/index.php
+++ Guias dps/tercer desafio practico/tercer-desafio/apiPhpDesafio3/index.php
@@ -7,11 +7,11 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 
 if($_SERVER['REQUEST_METHOD']=='GET'){
     if(isset($_GET['id'])){
-        $query="select * from alumno where id=".$_GET['id'];
+        $query="select * from ganancia where id=".$_GET['id'];
         $resultado=metodoGet($query);
         echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
     }else{
-        $query="select * from alumno";
+        $query="select * from ganancia";
         $resultado=metodoGet($query);
         echo json_encode($resultado->fetchAll()); 
     }
@@ -22,10 +22,10 @@ if($_SERVER['REQUEST_METHOD']=='GET'){
 if($_POST['METHOD']=='POST'){
     unset($_POST['METHOD']);
     $nombre=$_POST['nombre'];
-    $apellido=$_POST['apellido'];
-    $edad=$_POST['edad'];
-    $query="insert into alumno(nombre, apellido, edad) values ('$nombre', '$apellido', '$edad')";
-    $queryAutoIncrement="select MAX(id) as id from alumno";
+    $monto=$_POST['monto'];
+    $empleados=$_POST['empleados'];
+    $query="insert into ganancia(nombre, monto, empleados) values ('$nombre', $monto, '$empleados')";
+    $queryAutoIncrement="select MAX(id) as id from ganancia";
     $resultado=metodoPost($query, $queryAutoIncrement);
     echo json_encode($resultado);
     header("HTTP/1.1 200 OK");
@@ -36,9 +36,9 @@ if($_POST['METHOD']=='PUT'){
     unset($_POST['METHOD']);
     $id=$_GET['id'];
     $nombre=$_POST['nombre'];
-    $apellido=$_POST['apellido'];
-    $edad=$_POST['edad'];
-    $query="UPDATE alumno SET nombre='$nombre', apellido='$apellido', edad='$edad' WHERE id='$id'";
+    $monto=$_POST['monto'];
+    $empleados=$_POST['empleados'];
+    $query="UPDATE ganancia SET nombre='$nombre', monto=$monto, empleados='$empleados' WHERE id='$id'";
     $resultado=metodoPut($query);
     echo json_encode($resultado);
     header("HTTP/1.1 200 OK");
@@ -48,7 +48,7 @@ if($_POST['METHOD']=='PUT'){
 if($_POST['METHOD']=='DELETE'){
     unset($_POST['METHOD']);
     $id=$_GET['id'];
-    $query="DELETE FROM alumno WHERE id='$id'";
+    $query="DELETE FROM ganancia WHERE id='$id'";
     $resultado=metodoDelete($query);
     echo json_encode($resultado);
     header("HTTP/1.1 200 OK");
